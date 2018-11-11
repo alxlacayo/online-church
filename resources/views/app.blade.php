db5ac8201627488ff5eccec7ec7b7adbfdaa08de
Alex Lacayo <alex@alexlacayo.com>
2018-11-11 02:07:55 -0600

    Add meta title and remove comments

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,16 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    {{-- <meta name="viewport" content="width=device-width, initial-scale=1"> --}}
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
 
-    {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
-
-    <title>@yield('title')</title>
+    <title>Second Online</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    {{-- <link href="{{ asset('css/all.css') }}" rel="stylesheet"> --}}
     
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-1725903-8"></script>
